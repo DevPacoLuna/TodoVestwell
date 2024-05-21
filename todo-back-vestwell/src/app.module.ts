8c3ed3ea4e09abcc21fd10ae57949b702278d828
DevPacoLuna <dev.luna@outlook.com>
2024-05-21 14:13:51 -0600

    Add mysql and typeorm configuration

--- todo-back-vestwell/src/app.module.ts
+++ todo-back-vestwell/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: parseInt(process.env.MYSQL_TCP_PORT),
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
